e3ba3d6c29b25a22bd9bfd4a033d7a8ba6a92b51
Shade Webb <shadew@tutanota.com>
2019-03-31 21:09:21 -0400

    fix displayTodos method. Use functions as methods, arrow functions for callbacks

--- script.js
+++ script.js
@@ -2,13 +2,9 @@
 
 const todoList = {
     todos: ['test', 'test2', 'test3'],
-    //Is this code good enough? Can I pass "this" to an arrow function somehow? 
-    //Should a method use arrow functions?
     displayTodos: function () {
         this.todos.forEach(element => console.log(element));
     },
-    //Arrow function in strict mode "this" does not work, must use todoList
-    displayTodos2: () => todoList.todos.forEach(element => console.log(element))
 };
 
 // todoList.todos.push('test');
