64bebf38f27a6b1631a5fba25d35f3dee08b92bb
Shade Webb <shadew@tutanota.com>
2019-03-31 20:13:23 -0400

    two versions of displayTodos method using arrow functions and this

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict";
 
 const todoList = {
-    todos: [],
+    todos: ['test', 'test2', 'test3'],
+    //Is this code good enough? Can I pass "this" to an arrow function somehow? 
+    //Should a method use arrow functions?
     displayTodos: function () {
         this.todos.forEach(element => console.log(element));
-    }
+    },
+    //Arrow function in strict mode "this" does not work, must use todoList
+    displayTodos2: () => todoList.todos.forEach(element => console.log(element))
 };
 
-todoList.todos.push('test');
-todoList.todos.push('test2');
+// todoList.todos.push('test');
+// todoList.todos.push('test2');
 
 
-todoList.displayTodos();
+// todoList.displayTodos();
+
+todoList.displayTodos2();
